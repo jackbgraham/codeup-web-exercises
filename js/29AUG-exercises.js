096ed2ff7b5a5c413490617d72746717da95ee88
Jack Graham <jack.blair.graham@gmail.com>
2022-08-30 10:43:42 -0500

    css intro

---
+++ js/29AUG-exercises.js
@@ -0,0 +1,19 @@
+"use strict"
+//console.log("test");
+
+// Write a function, calculateTotalStudents, that returns the total number of students recorded in the classes array.
+let classes = [
+    {class: "6th grade history", students: 18},
+    {class: "7th grade history", students: 20},
+    {class: "8th grade history", students: 22}
+];
+
+function calculateTotalStudents(){
+
+}
+
+
+
+// If this stumps you , try starting with the simpler form of the problem: write a function that calculates the sum of the elements of an array.  Thus, if you give it
+// let numericArray = [8, 10, 20];
+// the function should return 38 (edited)
