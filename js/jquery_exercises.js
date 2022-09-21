22da9313b18996d8ece7b15cd3cfc295ecde22fa
Jack Graham <jack.blair.graham@gmail.com>
2022-09-21 10:51:46 -0500

    jQuery exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -3,3 +3,19 @@ console.log("test");
 $(function(){
 
 });
+
+$('#1').css('color', '#FDA687');
+$('#2').css('color', '#FC895F');
+$('#3').css('color', '#FB6B37');
+$('#4').css('color', '#FA4E0F');
+$('#5').css('color', '#DC3E04');
+$('#6').css('color', '#B43304');
+$('#7').css('color', '#8C2703');
+$('#8').css('color', '#641C02');
+$('#9').css('color', '#3C1101');
+
+$('.codeup').css({'border': '1px solid red', 'width': 'fit-content'});
+
+$('li').css('font-size', '20px');
+
+$('h1, p').css({'background-color': '#FFFF00'});
