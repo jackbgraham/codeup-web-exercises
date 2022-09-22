e1a87c69ec075d60fbd3803db08870c09495f422
Jack Graham <jack.blair.graham@gmail.com>
2022-09-22 11:21:06 -0500

    jquery mouse events

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,20 +2,42 @@
 console.log("test");
 $(function(){
 
+// $('#1').css('color', '#FDA687');
+// $('#2').css('color', '#FC895F');
+// $('#3').css('color', '#FB6B37');
+// $('#4').css('color', '#FA4E0F');
+// $('#5').css('color', '#DC3E04');
+// $('#6').css('color', '#B43304');
+// $('#7').css('color', '#8C2703');
+// $('#8').css('color', '#641C02');
+// $('#9').css('color', '#3C1101');
+//
+// $('.codeup').css({'border': '1px solid red', 'width': 'fit-content'});
+//
+// $('li').css('font-size', '20px');
+//
+// $('h1, p, li').css({'background-color': '#FFFF00'});
+
+    $('h1').click(function() {
+        $(this).css('font-style', 'italic');
+        $(this).css('background-color', '#FFFF00')
+    });
+
+    $('p').dblclick(function() {
+        $(this).css('font-size', '18px');
+    });
+
+    $('li').hover(
+        function() {
+            $(this).css('color', '#ff0000');
+        },
+        function() {
+            $(this).css('color', '#000000');
+        }
+    );
+
+
+
+
+
 });
-
-$('#1').css('color', '#FDA687');
-$('#2').css('color', '#FC895F');
-$('#3').css('color', '#FB6B37');
-$('#4').css('color', '#FA4E0F');
-$('#5').css('color', '#DC3E04');
-$('#6').css('color', '#B43304');
-$('#7').css('color', '#8C2703');
-$('#8').css('color', '#641C02');
-$('#9').css('color', '#3C1101');
-
-$('.codeup').css({'border': '1px solid red', 'width': 'fit-content'});
-
-$('li').css('font-size', '20px');
-
-$('h1, p, li').css({'background-color': '#FFFF00'});
