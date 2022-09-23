b2b38dbbeeaa8ef693556bcb47260d4d6b369f3d
Jack Graham <jack.blair.graham@gmail.com>
2022-09-23 11:23:59 -0500

    jquery national parks

---
+++ js/jquery_faq.js
@@ -0,0 +1,20 @@
+"use strict";
+console.log("test");
+$(function(){
+
+    $('button').css({
+        'background-color': '#4CAF50',
+        'border': 'none',
+        'color': 'white',
+        'padding': '5px 5px',
+        'text-align': 'center',
+        'text-decoration': 'none',
+        'display': 'inline-block',
+        'font-size': '16px'
+    });
+
+
+
+});
+
+
