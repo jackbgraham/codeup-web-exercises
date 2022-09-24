3842986c6e8c9363903b45337b7b528000599294
Jack Graham <jack.blair.graham@gmail.com>
2022-09-24 17:21:46 -0500

    jquery_faq completed

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -2,20 +2,144 @@
 console.log("test");
 $(function(){
 
+    $('dt').css({
+        'margin-to': '30px',
+        'margin-left': '80px'
+    });
+
     $('button').css({
         'background-color': '#4CAF50',
         'border': 'none',
+        'border-radius': '10px',
         'color': 'white',
-        'padding': '5px 5px',
+        'padding': '10px 5px',
         'text-align': 'center',
         'text-decoration': 'none',
         'display': 'inline-block',
-        'font-size': '16px'
+        'font-size': '16px',
+        'margin': '5px'
     });
 
-    $('#hidden').css({
-        'visibility': 'hidden'
+    $( "button" ).hover(
+        function() {
+            $( this ).css({
+                'background-color': 'white',
+                'border': '3px solid #4CAF50',
+                'border-radius': '10px',
+                'color': '#4CAF50',
+                'padding': '5px 5px',
+                'text-align': 'center',
+                'text-decoration': 'none',
+                'display': 'inline-block',
+                'font-size': '16px',
+                'margin': '5px'
+            });
+        }, function() {
+            $( this ).css({
+                'background-color': '#4CAF50',
+                'border': 'none',
+                'border-radius': '10px',
+                'color': 'white',
+                'padding': '10px 5px',
+                'text-align': 'center',
+                'text-decoration': 'none',
+                'display': 'inline-block',
+                'font-size': '16px',
+                'margin': '5px'
+            });
+        }
+    );
+
+    $( "button" ).hover(
+        function() {
+            $( this ).append( $( "<span> &#9759;</span>" ) );
+        }, function() {
+            $( this ).find( "span" ).last().remove();
+        }
+    );
+
+    $('#toggle1').hide();
+    $("#clickMe1").on(
+        'click', function(){
+        // alert('You clicked me');
+        $('#toggle1').toggle();
     });
+
+    $('#toggle2').hide();
+    $("#clickMe2").on(
+        'click', function(){
+            // alert('You clicked me');
+            $('#toggle2').toggle();
+        });
+
+    $('#toggle3').hide();
+    $("#clickMe3").on(
+        'click', function(){
+            // alert('You clicked me');
+            $('#toggle3').toggle();
+        });
+
+    $('#toggle4').hide();
+    $("#clickMe4").on(
+        'click', function(){
+            // alert('You clicked me');
+            $('#toggle4').toggle();
+        });
+
+    $('#toggle5').hide();
+    $("#clickMe5").on(
+        'click', function(){
+            // alert('You clicked me');
+            $('#toggle5').toggle();
+        });
+
+    $('#toggle6').hide();
+    $("#clickMe6").on(
+        'click', function(){
+            // alert('You clicked me');
+            $('#toggle6').toggle();
+        });
+
+    $('#toggle7').hide();
+    $("#clickMe7").on(
+        'click', function(){
+            // alert('You clicked me');
+            $('#toggle7').toggle();
+        });
+
+    $('#toggle8').hide();
+    $("#clickMe8").on(
+        'click', function(){
+            // alert('You clicked me');
+            $('#toggle8').toggle();
+        });
+
+    $('#toggle9').hide();
+    $("#clickMe9").on(
+        'click', function(){
+            // alert('You clicked me');
+            $('#toggle9').toggle();
+        });
+
+    $('#toggle10').hide();
+    $("#clickMe10").on(
+        'click', function(){
+            // alert('You clicked me');
+            $('#toggle10').toggle();
+        });
+
+    // $("#clickMe").on(
+    //     'click', function(){
+    //     // alert('You clicked me');
+    //     $('#toggle1').style('display': 'none')
+    // });
+
+    // this breaks it for some reason
+
+    // let admiralAccVar = $('#collapse').prev('h3');
+    // $("#accordion").accordion('option', 'active', admiralAccVar);
+
+    //accordion just stays white, which I think means it doesn't work anymore?
 
 
 });
