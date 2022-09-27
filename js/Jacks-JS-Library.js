8d697268917d8e5c9e79cdd32ad7385d23211ee2
Jack Graham <jack.blair.graham@gmail.com>
2022-09-27 14:02:15 -0500

    map and wx api now functioning

--- js/Jacks-JS-Library.js
+++ js/Jacks-JS-Library.js
@@ -8,3 +8,22 @@ function sortNumArray(array){
 // let testArray = [5, 6, 1, 2, 3, 4];
 // testArray = testArray.sort(function(a, b){return a - b;});
 // outputs [1, 2, 3, 4, 5, 6]
+
+//removeX(array, x) takes in an array, and an element from the array, then returns a new array with that element removed
+function removeX(array, x){
+    return array.filter(element => {
+       return element !==x;
+    });
+}
+
+//takes in a multi-word string and splits it into an array
+function stringToArray(str){
+    return {
+        firstWord: string.split(' ')[0],
+        secondWord: string.split(' ')[1]
+    }
+}
+
+// this is the spread operator [...str], it converts back and forth between strings and arrays, str here is a placeholder
+
+
--- js/weather_map.js
+++ js/weather_map.js
@@ -1,11 +1,21 @@
+"use strict";
 $(function(){
 
-    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+    // $.get("http://api.openweathermap.org/data/2.5/onecall", {
+    //     APPID: OPEN_WX_APPID,
+    //     lat: 29.423,
+    //     long: -98.485,
+    //     units: "imperial"
+    // }).done(function(data) {
+    //     console.log('the entire response:', data);
+    // });
+
+    $.get("https://api.openweathermap.org/data/2.5/forecast", {
         APPID: OPEN_WX_APPID,
         lat: 29.423,
         long: -98.485,
         units: "imperial"
     }).done(function(data) {
-        console.log('the entire response:', data);
+        console.log(data);
     });
 })
