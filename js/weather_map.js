c376f38a98a45cc6332dfc0717fd3bac7d7882d1
Jack Graham <jack.blair.graham@gmail.com>
2022-09-29 09:31:41 -0500

    forecast tables working

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,28 +1,96 @@
 "use strict";
 $(function(){
+
     //forecast data
     $.get("https://api.openweathermap.org/data/2.5/forecast", {
         APPID: OPEN_WX_APPID,
-        lat: 29.423,
-        lon: -98.485,
+        lat: markerLocation.lat,
+        lon: markerLocation.lng,
         units: "imperial"
     }).done(function(data) {
         console.log("forecast data")
-        console.log(data);
+        console.log(data.list);
+        console.log(data.list[0].dt_txt)
+        var iconcode1 = data.list[0].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode1 + ".png";
+        var iconcode2 = data.list[8].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode2 + ".png";
+        var iconcode3 = data.list[16].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode3 + ".png";
+        var iconcode4 = data.list[24].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode4 + ".png";
+        var iconcode5 = data.list[32].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode5 + ".png";
+        $(`#forecast1Display1`).html(data.list[0].dt_txt);
+        $(`#forecast1Display2`).html('temp: ' + data.list[0].main.temp_min + ' - ' + data.list[0].main.temp_max);
+        $(`#forecast1Display3`).html(data.list[0].weather[0].description + " ").append( `<br>`).append( `<img src="${iconurl}">`);
+        $(`#forecast1Display4`).html(data.list[0].main.humidity + "% rh");
+        $(`#forecast1Display5`).html("wind " + data.list[0].wind.speed + "mph / " + windCardinalDirection(data.list[0].wind.deg));
+
+        $(`#forecast2Display1`).html(data.list[8].dt_txt);
+        $(`#forecast2Display2`).html('temp: ' + data.list[8].main.temp_min + ' - ' + data.list[8].main.temp_max);
+        $(`#forecast2Display3`).html(data.list[8].weather[0].description + " ").append( `<br>`).append( `<img src="${iconurl}">`);
+        $(`#forecast2Display4`).html(data.list[8].main.humidity + "% rh");
+        $(`#forecast2Display5`).html("wind " + data.list[8].wind.speed + "mph / " + windCardinalDirection(data.list[8].wind.deg));
+
+        $(`#forecast3Display1`).html(data.list[16].dt_txt);
+        $(`#forecast3Display2`).html('temp: ' + data.list[16].main.temp_min + ' - ' + data.list[16].main.temp_max);
+        $(`#forecast3Display3`).html(data.list[16].weather[0].description + " ").append( `<br>`).append( `<img src="${iconurl}">`);
+        $(`#forecast3Display4`).html(data.list[16].main.humidity + "% rh");
+        $(`#forecast3Display5`).html("wind " + data.list[16].wind.speed + "mph / " + windCardinalDirection(data.list[16].wind.deg));
+
+        $(`#forecast4Display1`).html(data.list[24].dt_txt);
+        $(`#forecast4Display2`).html('temp: ' + data.list[24].main.temp_min + ' - ' + data.list[24].main.temp_max);
+        $(`#forecast4Display3`).html(data.list[24].weather[0].description + " ").append( `<br>`).append( `<img src="${iconurl}">`);
+        $(`#forecast4Display4`).html(data.list[24].main.humidity + "% rh");
+        $(`#forecast4Display5`).html("wind " + data.list[24].wind.speed + "mph / " + windCardinalDirection(data.list[24].wind.deg));
+
+        $(`#forecast5Display1`).html(data.list[32].dt_txt);
+        $(`#forecast5Display2`).html('temp: ' + data.list[32].main.temp_min + ' - ' + data.list[32].main.temp_max);
+        $(`#forecast5Display3`).html(data.list[32].weather[0].description + " ").append( `<br>`).append( `<img src="${iconurl}">`);
+        $(`#forecast5Display4`).html(data.list[32].main.humidity + "% rh");
+        $(`#forecast5Display5`).html("wind " + data.list[32].wind.speed + "mph / " + windCardinalDirection(data.list[32].wind.deg));
+
     });
 
     // current wx data
     $.get("https://api.openweathermap.org/data/2.5/weather", {
         APPID: OPEN_WX_APPID,
-        lat: 29.423,
-        lon: -98.485,
+        lat: markerLocation.lat,
+        lon: markerLocation.lng,
         units: "imperial"
     }).done(function(data) {
         console.log("current wx data");
         console.log(data);
+        //for the current wx icon
+        var iconcode = data.weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        $(`#locationDisplay1`).html("City: " + data.name + ", " + data.sys.country);
+        $(`#locationDisplay2`).html("Currently: " + data.weather[0].description + " " ).append( `<img src="${iconurl}">`);
+        $(`#locationDisplay3`).html("Visibility: " + data.visibility + "m");
+        $(`#locationDisplay4`).html("Wind from the " + windCardinalDirection(data.wind.deg) + " at " + data.wind.speed + "mph");
+        $(`#locationDisplay5`).html("Temp: " + data.main.temp + "° F");
+        $(`#locationDisplay6`).html("Range: " + data.main.temp_min + "° F - " + data.main.temp_max + "° F");
+        $(`#locationDisplay7`).html("Humidity: " + data.main.humidity + "%rh");
+        });
+
+
+
     });
 
-    // console.log(lngLat);
+function updateLocation(){
+    $.get("https://api.openweathermap.org/data/2.5/weather", {
+        APPID: OPEN_WX_APPID,
+        lat: markerLocation.lat,
+        lon: markerLocation.lng,
+        units: "imperial"
+    }).done(function(data) {
+
+    });
+};
+
+
+
 
     //convert azimuth to cardinal directions function
     function windCardinalDirection(degrees){
@@ -93,7 +161,7 @@ function namedDayFromDay(timeStamp){
 
 
 
-})
+
 //mapbox notes
 //Forward Geocoding - search by address
 //Reverse Geocoding - search by coordinates
