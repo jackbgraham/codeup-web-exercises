931081a54bd092ccef3f981c17475297151988e7
Jack Graham <jack.blair.graham@gmail.com>
2022-10-04 12:07:09 -0500

    promises is accessing github

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,21 @@
 console.log('test');
+
+// fetch('https://github.com/jackbgraham', {headers: {
+//     `Authorization`: `token ${GITHUB_TOKEN}`
+//     }})
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+
+function getUsersLastCommit(username) {
+    fetch(`https://api.github.com/users/${username}/events/public`, {
+        headers: {
+            'Authorization': `token ${GITHUB_TOKEN}`
+        }
+    }).then(response => response.json())
+        .then(events => {
+            console.log(events)
+            console.log(events[0].created_at)})
+        .catch(error => console.error(error));
+}
+
+getUsersLastCommit('jackbgraham');
